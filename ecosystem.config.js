a5bb930a9dba4d318ac209a2587cf7216aff356f
samip <kothiyasamip@gmail.com>
2023-07-28 23:31:06 +0530

    add config setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     apps: [
       {
-        name: "API",
+        name: "EXCH_B2B_MASTER_ADMIN",
         script: "npm",
         cwd: ".",
-        automation: false,
+        time: true,
         args: "run start",
         max_memory_restart: "1G",
         watch: false,
         log_date_format: "DD-MM HH:mm:ss.SSS",
-        autorestart: false,
+        autorestart: true,
         env: {
           PORT: 5000,
           NODE_ENV: "development",
