c8adb66a96da31dc901362bfd8419838b18aeaba
samip <kothiyasamip@gmail.com>
2023-08-05 16:20:23 +0530

    update userid

--- src/modules/activity/activity.controller.ts
+++ src/modules/activity/activity.controller.ts
@@ -7,20 +7,14 @@ import prepareResponse from "@/utils/prepareResponse";
 import * as ActivityService from "./activity.service";
 import httpStatus from "http-status";
 const fetchActivity = catchAsync(
-  async (req: Request, res: CustomResponse) => {
-    try {
+  async (req: Request, res: CustomResponse) => {    
       const { userId } = req.body
       const data = ActivityService.fetchActivity(req.user, userId);
       const response = prepareResponse({
         message: "Activity success",
         data,
       });
-      res.status(httpStatus.OK).json(response);
-    }
-    catch (error) {
-      console.log("error",error);
-      
-    }
+      res.status(httpStatus.OK).json(response);    
   }
 );
 
--- src/modules/activity/activity.service.ts
+++ src/modules/activity/activity.service.ts
@@ -4,23 +4,30 @@ import httpStatus from "http-status";
 
 const fetchActivity = async (data: any, userId: string): Promise<void> => {
   let username = data.username
-  if (userId) {
-    const userData: any = await User.findOne({ _id: userId });
-    if (!userData) {
-      throw new ApiError(httpStatus.BAD_REQUEST, {
-        msg: "user not found",
-      });
+  try {
+    if (userId) {
+      const userData: any = await User.findOne({ _id: userId });
+      if (!userData) {
+        throw new ApiError(httpStatus.BAD_REQUEST, {
+          msg: "user not found",
+        });
+      }
+
+      if (!userData.parentId == userData._id) {
+        throw new ApiError(httpStatus.BAD_REQUEST, {
+          msg: "you are not refered to this user.",
+        });
+      }
+      username = userData.username
     }
 
-    if (!userData.parentId == userData._id) {
-      throw new ApiError(httpStatus.BAD_REQUEST, {
-        msg: "you are not refered to this user.",
-      });
-    }
-    username = userData.username
+    const response = await Activity.find({ username });
+    return response;
+  } catch (error) {
+    throw new ApiError(httpStatus.BAD_REQUEST, {
+          msg: "invalid user id.",
+        });
+    
   }
-  
-  const response = await Activity.find({ username });
-  return response;
 }
 export { fetchActivity }
