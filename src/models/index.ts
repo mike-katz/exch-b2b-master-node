259f2b11f5b0c7d3bc1607b427c5b52a1b0b0ce6
Abhinav <137391551+abhinav-raj-freelancer@users.noreply.github.com>
2023-12-03 01:59:34 +0530

    Revert "Merge pull request #6 from mike-katz/exposure-list-api"
    This reverts commit 33528789203f6b377a9d881acd6e78df027ec1ea, reversing
    changes made to 7c2890cf1d832ee581f5f279982ee1599edd1161.

--- src/models/index.ts
+++ src/models/index.ts
@@ -22,3 +22,4 @@ export { default as AuraCSPlaceBet } from "./auraCSPlaceBet.model";
 export { default as AuraCSResult } from "./auraCSResult.model";
 export { default as SoccerBetPlace } from "./soccerBetPlace.model";
 export { default as TennisBetPlace } from "./tennisBetPlace.model";
+export { default as ExposureManage } from "./exposuremanage.model";
